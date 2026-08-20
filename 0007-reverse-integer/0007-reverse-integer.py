00f859ee727c4817e4d4f945b5b5dba309d3537f
Subham Dani <subhamdani7781@gmail.com>
2026-08-21 01:18:14 +0530

    Time: 45 ms (63.86%), Space: 19.1 MB (70.46%) - LeetHub

--- 0007-reverse-integer/0007-reverse-integer.py
+++ 0007-reverse-integer/0007-reverse-integer.py
@@ -4,13 +4,8 @@ class Solution:
             sign=-1
         else:
             sign=1
-        x=abs(x)
-        rev=0
-        while x>0:
-            digit=x%10
-            rev=rev*10+digit
-            x=x//10
-        rev=rev*sign
+        x=str(abs(x))
+        rev=int(x[::-1])*sign
         if rev< -2**31 or rev> 2**31-1:
             return 0
         return rev
